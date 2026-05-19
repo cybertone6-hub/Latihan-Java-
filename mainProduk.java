428a9e93f9bd434859185f0227d993cdb120a37e
Muhammad Lutvi <cybertone6@gmail.com>
2026-05-19 10:41:14 +0700

    Add files via upload

---
+++ mainProduk.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanPBO;
+
+/**
+ *
+ * @author LUTVI
+ */
+import java.util.Scanner;
+import latihanPBO.Produk;
+
+public class mainProduk {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        mainProduk bayar = new mainProduk();
+       
+        System.out.println("=== Input Data Produk ===");
+        
+        System.out.print("Masukkan kode produk   : ");
+       // bayar.setHarga(input.nextLine());
+        
+        //int Harga = pp1.getHarga();
+       // System.out.println("belanjaan");
+        //System.out.println("kode produk  = "+bayar.getkodeProduk());
+        //System.out.println("nama produk    = "+bayar.getnamaProduk());
+        //System.out.println("harga     = "+bayar.getharga());
+       // System.out.println("stock = "+bayar.getStock());
+        
+        
+        }
+}
